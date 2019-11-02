38a3ae0800d2265bf53ab45a426dfaaeab67cb2f
hakase0724 <35761029+hakase0724@users.noreply.github.com>
2019-11-02 13:46:43 +0900

    commit my scriptfiles

---
+++ GitScript/NaritaScript/MouseCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamera : MonoBehaviour
+{
+    public Transform YAxis;
+    public Transform XAxis;
+
+    public float XSpeed = 1.0f;
+    public float YSpeed = 1.0f;
+
+    private void Start()
+    {
+        //マウスカーソル消したければこのコメントアウトを外す
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float X_Rotation = Input.GetAxis("Mouse X") * XSpeed;
+        float Y_Rotation = Input.GetAxis("Mouse Y") * YSpeed;
+        YAxis.transform.Rotate(0, -X_Rotation, 0);
+        XAxis.transform.Rotate(Y_Rotation, 0, 0);
+    }
+}
---
+++ GitScript/NaritaScript/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float XSpeed = 1.0f;
+    public float YSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos += transform.right * Input.GetAxis("Horizontal") * XSpeed;
+        pos += transform.forward * Input.GetAxis("Vertical") * YSpeed;
+        transform.position = pos;
+    }
+}
